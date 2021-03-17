e80fac456cb20d2e43ab12763315ad340df057b2
Alex Hunt <hello@alexhunt.io>
2021-03-17 07:32:22 +0000

    Refine base ESLint config, add Prettier to CI lint job

--- scripts/eslint/base.js
+++ scripts/eslint/base.js
@@ -4,24 +4,30 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json',
   },
-  extends: [
-    'eslint:recommended',
-    'prettier',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
+  extends: ['eslint:recommended'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      rules: {
+        // Allow use of `@ts-ignore` comments
+        '@typescript-eslint/ban-ts-ignore': 'off',
+
+        // Allow return types to be inferred
+        '@typescript-eslint/explicit-function-return-type': 'off',
+
+        // Allow implicit types at module boundaries
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+        // Disallow unhandled promises, requiring `void` statement
+        '@typescript-eslint/no-floating-promises': [
+          'error',
+          { ignoreVoid: true },
+        ],
+      },
+    },
   ],
-  rules: {
-    // Allow use of `@ts-ignore` comments
-    '@typescript-eslint/ban-ts-ignore': 'off',
-
-    // Allow return types to be inferred
-    '@typescript-eslint/explicit-function-return-type': 'off',
-
-    // Allow implicit types at module boundaries
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-
-    // Disallow unhandled promises, requiring `void` statement
-    '@typescript-eslint/no-floating-promises': ['error', { ignoreVoid: true }],
-  },
 };

82d4fe958b36566cd4bbb469f3bffdef1d679bab
Alex Hunt <hello@alexhunt.io>
2021-03-17 07:16:44 +0000

    Include react-notion-x, update homepage to display bio fragment (#2)

--- site/src/pages/_app.tsx
+++ site/src/pages/_app.tsx
@@ -1,5 +1,9 @@
-import { AppProps } from 'next/app';
+import type { AppProps } from 'next/app';
+
+// organize-imports-ignore
 import '~styles/globals.css';
+import 'react-notion-x/src/styles.css';
+import '~notion/style-overrides.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;

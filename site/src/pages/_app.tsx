a9dfbd0525ccdf67164c542b6634144e17d8c23d
Alex Hunt <hello@alexhunt.io>
2021-03-27 14:53:20 +0000

    Configure breakpoint properties in stylesheets

--- site/src/pages/_app.tsx
+++ site/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
 // organize-imports-ignore
+import '~styles/theme.css';
 import '~styles/globals.css';
 import 'react-notion-x/src/styles.css';
 import '~notion/style-overrides.css';

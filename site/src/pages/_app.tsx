c7d839c06de969b531105dd1fcb0560317d3e30d
Alex Hunt <hello@alexhunt.io>
2021-04-05 20:15:40 +0100

    Adjust Gruvbox colours, enable JSON highlighting

--- site/src/pages/_app.tsx
+++ site/src/pages/_app.tsx
@@ -14,6 +14,7 @@ import 'prismjs/components/prism-markup-templating';
 import 'prismjs/components/prism-bash';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-json';
 import 'prismjs/components/prism-php';
 import 'prismjs/components/prism-python';
 

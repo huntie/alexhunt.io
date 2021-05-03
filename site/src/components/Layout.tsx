3f7c4d720cd36bd746d8c2363646cacb35a89b9b
Alex Hunt <hello@alexhunt.io>
2021-05-03 15:05:01 +0100

    Add site footer (#16)

--- site/src/components/Layout.tsx
+++ site/src/components/Layout.tsx
@@ -1,6 +1,11 @@
 import type { ReactNode } from 'react';
 import useDarkMode from 'use-dark-mode';
+import packageInfo from '../../package.json';
+import Footer from './Footer';
 import Header from './Header';
+
+const REPO_URL = packageInfo.repository.url;
+const CHANGELOG_URL = REPO_URL + '/pulls?q=is%3Apr+is%3Amerged';
 
 type Props = {
   children: ReactNode;
@@ -20,6 +25,11 @@ const Layout = ({ children }: Props) => {
         onDarkModeChange={darkMode.toggle}
       />
       {children}
+      <Footer
+        copyrightName="Alex Hunt"
+        repositoryUrl={REPO_URL}
+        changelogUrl={CHANGELOG_URL}
+      />
     </>
   );
 };

2789f38a8e0716366c9bc34d5c1836eb996ac453
Alex Hunt <hello@alexhunt.io>
2021-05-15 11:12:25 +0100

    Add custom CodeBlock component with copy button

--- site/src/components/NotionRenderer.tsx
+++ site/src/components/NotionRenderer.tsx
@@ -1,5 +1,6 @@
 import type { ComponentProps } from 'react';
 import { NotionRenderer as BaseNotionRenderer } from 'react-notion';
+import CodeBlock from './CodeBlock';
 
 const mapImageUrl = (url: string) => {
   const imagePath = url.split('amazonaws.com/secure.notion-static.com/')[1];
@@ -7,8 +8,18 @@ const mapImageUrl = (url: string) => {
   return `/static/${imagePath.substring(0, 36)}.${imagePath.split('.')[1]}`;
 };
 
-const NotionRenderer = (props: ComponentProps<typeof BaseNotionRenderer>) => (
-  <BaseNotionRenderer {...props} mapImageUrl={mapImageUrl} />
+const NotionRenderer = ({
+  customBlockComponents,
+  ...props
+}: ComponentProps<typeof BaseNotionRenderer>) => (
+  <BaseNotionRenderer
+    {...props}
+    mapImageUrl={mapImageUrl}
+    customBlockComponents={{
+      ...customBlockComponents,
+      code: CodeBlock,
+    }}
+  />
 );
 
 export default NotionRenderer;

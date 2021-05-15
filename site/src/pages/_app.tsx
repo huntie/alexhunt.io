8d0c8087dce7f9bbe52d24385046749ba859f589
Alex Hunt <hello@alexhunt.io>
2021-05-15 17:45:51 +0100

    Update code highlighting to handle JSX and plain text (#20)

--- site/src/pages/_app.tsx
+++ site/src/pages/_app.tsx
@@ -13,10 +13,11 @@ import 'prismjs';
 import 'prismjs/components/prism-markup-templating';
 import 'prismjs/components/prism-bash';
 import 'prismjs/components/prism-css';
-import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-jsx';
 import 'prismjs/components/prism-php';
 import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-typescript';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
